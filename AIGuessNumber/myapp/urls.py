9cb20ab952a4e78c48bc40d38c1ceff09ec13b5f
webdeveloper510 <developerweb6@gmail.com>
2023-03-28 15:50:55 +0530

    changes

--- AIGuessNumber/myapp/urls.py
+++ AIGuessNumber/myapp/urls.py
@@ -2,10 +2,10 @@ from django.urls import path,include
 from . import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
-      path('save/',views.SaveDAtabase,name='save_random_arrays'),
-      # path("" , views.home),
-      # path('input/' ,views.add_new_sequence),
-      # path('history/' ,views.array_history),
-      # path('reset/' ,views.Reset_history),
+      # path('save/',views.SaveDAtabase,name='save_random_arrays'),
+      path("" , views.home),
+      path('input/' ,views.add_new_sequence),
+      path('history/' ,views.array_history),
+      path('reset/' ,views.Reset_history),
       ]
 urlpatterns += staticfiles_urlpatterns()
